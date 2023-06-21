2ea599f11c4bdcfcb7f5ecb0d40a94064b4a4e96
wyy_fatcat <319226862@qq.com>
2023-06-21 09:40:11 +0800

    changeName

---
+++ .history/LeetCode/2619_20230620213940.js
@@ -0,0 +1,6 @@
+// 请你编写一段代码实现一个数组方法，
+// 使任何数组都可以调用 array.last() 方法，
+// 这个方法将返回数组最后一个元素。如果数组中没有元素，则返回 -1 。
+Array.prototype.last = function() {
+    return this.length < 1 ? -1:this[this.length-1]
+};
---
+++ .history/LeetCode/2620_20230621092418.js
@@ -0,0 +1,22 @@
+// 请你编写并返回一个 计数器 函数，
+// 它接收一个整型参数 n 。
+// 这个 计数器 函数最初返回 n，、
+// 每次调用它时返回前一个值加 1 的值 ( n ,  n + 1 ,  n + 2 ，等等)。
+
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+// 闭包
+var createCounter = function(n) {
+    return function() {
+        return n++
+    };
+};
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
